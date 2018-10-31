a811785cc935ab04982e47b6a1f74796dab7a130
Adam <spincycle01@yahoo.com>
2018-10-31 17:51:01 -0400

    Initial homework
    Upload Hello Android World initial

---
+++ HelloAndroidWorld/app/src/main/java/com/example/adamhome/helloandroidworld/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.adamhome.helloandroidworld
+//this file houses the Kotlin program for the app.  It controls the behavior of the app.
+
+
+import android.app.Activity
+import android.os.Bundle
+
+//no changes have been made to the Kotlin file
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
